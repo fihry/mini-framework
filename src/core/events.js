38d7c53a2117ed714a19abe9b3166b1d1f89f006
obenali <oussama.benali@ump.ac.ma>
2025-05-23 10:58:06 +0100

    change the event manager to capture custom event and add addeventlistener to the on method so it captures the event automatically

--- src/core/events.js
+++ src/core/events.js
@@ -1,21 +1,46 @@
 class EventManager {
-  constructor() {
+  constructor() { 
     this.customEvents = {};
+    this.nativeListeners = new Map(); // Tracks native event handlers
   }
 
-  on(eventName, callback) {
-    if (!this.customEvents[eventName]) {
-      this.customEvents[eventName] = [];
+  isNativeEvent(eventName) {
+    const nativeEvents = ['keydown', 'keyup', 'click', 'scroll', 'mousedown', 'mouseup'];
+    return nativeEvents.includes(eventName);
+  }
+
+  on(eventName, callback, element = window) {
+    if (!this.customEvents[eventName]) { // the event isn't registered
+      this.customEvents[eventName] = []; // register the event
+
+      // Setup native event listener if applicable
+      if (this.isNativeEvent(eventName)) {
+        const handler = (e) => this.emit(eventName, e);
+        element.addEventListener(eventName, handler);
+        this.nativeListeners.set(eventName, { element, handler });
+      }
     }
-    this.customEvents[eventName].push(callback);
+    // event registered
+    this.customEvents[eventName].push(callback); // overite the event 
   }
 
   off(eventName, callback) {
     if (!this.customEvents[eventName]) return;
 
+    // Remove the callback
     this.customEvents[eventName] = this.customEvents[eventName].filter(
       cb => cb !== callback
     );
+
+    // If no more callbacks, remove native listener
+    if (this.customEvents[eventName].length === 0) {
+      const nativeListener = this.nativeListeners.get(eventName);
+      if (nativeListener) {
+        nativeListener.element.removeEventListener(eventName, nativeListener.handler);
+        this.nativeListeners.delete(eventName);
+      }
+      delete this.customEvents[eventName];
+    }
   }
 
   emit(eventName, data) {
@@ -25,5 +50,6 @@ class EventManager {
     }
   }
 }
+
 
 export default EventManager;
